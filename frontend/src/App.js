88463767be3616a65bf9715dd125661beb84fdfd
karl <karlkivisild@gmail.com>
2024-10-10 08:57:07 +0300

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tooted, setTooted] = useState([]);
+  const idRef = useRef();
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const isActiveRef = useRef();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/tooted")
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }, []);
+
+  function kustuta(index) {
+    fetch("http://localhost:3000/kustuta-toode/" + index)
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+
+  function lisa() {
+    fetch(`http://localhost:4444/tooted/lisa/${Number(idRef.current.value)}/${nameRef.current.value}/${Number(priceRef.current.value)}/${isActiveRef.current.checked}`)
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+
+  function dollariteks() {
+    const kurss = 1.1;
+    fetch("http://localhost:4444/tooted/hind-dollaritesse/" + kurss)
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+
+  return (
+    <div className="App">
+      <label>ID</label> <br />
+      <input ref={idRef} type="number" /> <br />
+      <label>Nimi</label> <br />
+      <input ref={nameRef} type="text" /> <br />
+      <label>Hind</label> <br />
+      <input ref={priceRef} type="number" /> <br />
+      <label>Aktiivne</label> <br />
+      <input ref={isActiveRef} type="checkbox" /> <br />
+      <button onClick={() => lisa()}>Lisa</button>
+      {tooted.map((toode, index) => 
+        <div>
+          <div>{toode.id}</div>
+          <div>{toode.name}</div>
+          <div>{toode.price}</div>
+          <button onClick={() => kustuta(index)}>x</button>
+        </div>)}
+      <button onClick={() => dollariteks()}>Muuda dollariteks</button>
+    </div>
+  );
+}
+
+export default App;
--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import stringsController from "./controllers/strings";
-import productsController from "./controllers/products"
-import productslistController from "./controllers/productslist"
+import productsController from "./controllers/products";
+import productListController from "./controllers/productslist";
 
 const app: Express = express();
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
 app.use('/', stringsController);
-
-app.use('/', productsController); 
-
-app.use('/', productslistController)
+app.use('/', productsController);
+app.use('/', productListController);
 
 app.listen(3000,() => {
   console.log(`[server]: Server is running at http://localhost:3000`);
